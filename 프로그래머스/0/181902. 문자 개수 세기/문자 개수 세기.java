6a3cba93c6a8ec344ae02a9e5ea16c25670f8ace
Kim Ki Ug <2897robo@naver.com>
2024-03-11 09:25:35 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.02 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(String my_string) {
+        int[] answer = new int[52];
+        for(char c : my_string.toCharArray()) {
+            if(c >= 'a' && c<= 'z') {
+                answer[26+c-'a']++;
+            } else if (c >= 'A' && c <= 'Z') {
+                answer[c-'A']++;
+            }
+        }
+        
+        return answer;
+    }
+}
