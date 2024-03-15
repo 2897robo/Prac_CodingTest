308f455ff29c7424eba677397747fecedbf642ff
Kim Ki Ug <2897robo@naver.com>
2024-03-15 09:09:08 +0900

    [level 0] Title: A 강조하기, Time: 0.02 ms, Memory: 69.5 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.java
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -1,13 +1,9 @@
 class Solution {
+
     public String solution(String myString) {
-        StringBuilder answer = new StringBuilder(myString.toLowerCase());
-        
-        for (int i = 0; i < answer.length(); i++) {
-            if (answer.charAt(i) == 'a') {
-                answer.setCharAt(i, Character.toUpperCase('a'));
-            }
-        }
-        
-        return answer.toString();
+        myString = myString.toLowerCase();
+        myString = myString.replace('a', 'A');
+
+        return myString;
     }
 }
