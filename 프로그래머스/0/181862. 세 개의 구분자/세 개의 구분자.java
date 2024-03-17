b3473ecb97edc357f13d6f82996046de348c85e7
Kim Ki Ug <2897robo@naver.com>
2024-03-17 18:24:37 +0900

    [level 0] Title: 세 개의 구분자, Time: 32.68 ms, Memory: 93.9 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -1,33 +1,22 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.StringTokenizer;
 
 class Solution {
     public String[] solution(String myStr) {
-        List<String> result = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
-
-        // 문자열을 순회하면서 "a", "b", "c"를 구분자로 사용해 문자열을 나눕니다.
-        for (char c : myStr.toCharArray()) {
-            if (c == 'a' || c == 'b' || c == 'c') {
-                if (sb.length() > 0) {
-                    result.add(sb.toString());
-                    sb.setLength(0); // StringBuilder를 비웁니다.
-                }
-            } else {
-                sb.append(c);
+        String[] answer = {};
+        
+        StringTokenizer st = new StringTokenizer(myStr,"abc");
+        
+        if (st.countTokens() != 0) {
+            answer = new String[st.countTokens()];
+            for(int i=0; i<answer.length; i++){
+                answer[i] = st.nextToken();
             }
         }
-
-        // 남아 있는 문자열이 있다면 결과 리스트에 추가합니다.
-        if (sb.length() > 0) {
-            result.add(sb.toString());
+        else {
+            answer = new String[1];
+            answer[0] = "EMPTY";
         }
 
-        // 결과 리스트를 배열로 변환하여 반환합니다.
-        if (result.isEmpty()) {
-            return new String[]{"EMPTY"};
-        } else {
-            return result.toArray(new String[0]);
-        }
+        return answer;
     }
 }
