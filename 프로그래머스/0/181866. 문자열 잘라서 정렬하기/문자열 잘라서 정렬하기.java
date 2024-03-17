958d1f4b484965e4fca5540b524f55aaaf37558b
Kim Ki Ug <2897robo@naver.com>
2024-03-17 17:18:05 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 15.13 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
@@ -2,15 +2,14 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String myString) {
-        String[] parts = myString.split("x");
-        List<String> resultList = new ArrayList<>();
-        for (String part : parts) {
-            if (!part.isEmpty()) {
-                resultList.add(part);
-            }
-        }
-        String[] resultArray = resultList.toArray(new String[0]);
-        Arrays.sort(resultArray);
-        return resultArray;
+        StringTokenizer st = new StringTokenizer(myString, "x");
+        int idx = 0, N = st.countTokens();
+        String[] result = new String[N];
+        
+        for (int n = 0;n < N;n++)
+            result[idx++] = st.nextToken();
+        
+        Arrays.sort(result);
+        return result;
     }
 }
