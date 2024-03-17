dd549622d3fb45032550685abb345a24818f09f0
Kim Ki Ug <2897robo@naver.com>
2024-03-17 18:18:54 +0900

    [level 0] Title: rny_string, Time: 0.03 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/0/181863. rny＿string/rny＿string.java
+++ 프로그래머스/0/181863. rny＿string/rny＿string.java
@@ -1,18 +1,6 @@
 class Solution {
     public String solution(String rny_string) {
-        // 문자열을 StringBuilder로 변환하여 수정 가능하도록 합니다.
-        StringBuilder sb = new StringBuilder(rny_string);
-        
-        // 문자열을 순회하면서 'm'을 'rn'으로 대체합니다.
-        for (int i = 0; i < sb.length(); i++) {
-            if (sb.charAt(i) == 'm') {
-                sb.replace(i, i + 1, "rn");
-                // 'm'을 'rn'으로 대체했으므로 인덱스를 한 칸 뒤로 이동합니다.
-                i++;
-            }
-        }
-        
-        // StringBuilder를 다시 String으로 변환하여 반환합니다.
-        return sb.toString();
+        String answer = rny_string.replace("m", "rn");
+        return answer;
     }
 }
