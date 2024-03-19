a88099589e27a5e4c8fef1bbfda33e54b528a06d
Kim Ki Ug <2897robo@naver.com>
2024-03-20 08:45:26 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.63 ms, Memory: 71.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
@@ -0,0 +1,14 @@
+import java.util.PriorityQueue;
+
+class Solution {
+    public int solution(int[] rank, boolean[] attendance) {
+
+        PriorityQueue<Integer> que = new PriorityQueue<>((a, b) -> rank[a] - rank[b]);
+        for (int i = 0; i < attendance.length; i++) {
+            if (attendance[i])
+                que.add(i);
+        }
+
+        return que.poll() * 10000 + que.poll() * 100 + que.poll();
+    }
+}
