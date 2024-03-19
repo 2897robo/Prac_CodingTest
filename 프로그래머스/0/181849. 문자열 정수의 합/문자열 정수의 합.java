5f8d28176016402ae7bbc92877bb6f337cf69ad8
Kim Ki Ug <2897robo@naver.com>
2024-03-19 12:44:55 +0900

    [level 0] Title: 문자열 정수의 합, Time: 0.23 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.java
+++ 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.java
@@ -1,12 +1,10 @@
 class Solution {
     public int solution(String num_str) {
-        long answer = 0; // 누적 변수의 자료형을 long으로 변경
-        for(int i = 0; i < num_str.length(); i++) {
-            char digit = num_str.charAt(i);
-            if(Character.isDigit(digit)) { // 숫자 문자인 경우에만 처리
-                answer += Character.getNumericValue(digit);
-            }
+        int answer = 0;
+        String[] numArr= num_str.split("");
+        for (String num : numArr){
+            answer+=Integer.parseInt(num);
         }
-        return (int)answer; // long을 int로 변환하여 반환
+        return answer;
     }
 }
