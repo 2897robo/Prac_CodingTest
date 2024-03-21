4d7a4115101dbe4307fc0e99a2bd625ee240221e
Kim Gi Uk <2897robo@naver.com>
2024-03-21 09:01:21 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.68 ms, Memory: 70.4 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
@@ -1,6 +1,13 @@
+import java.time.LocalDate;
+
 class Solution {
+
     public int solution(int[] date1, int[] date2) {
-        if (date1[0] < date2[0] || (date1[0] == date2[0] && date1[1] < date2[1]) || (date1[0] == date2[0] && date1[1] == date2[1] && date1[2] < date2[2])) {
+
+        LocalDate dateA = LocalDate.of(date1[0], date1[1], date1[2]);
+        LocalDate dateB = LocalDate.of(date2[0], date2[1], date2[2]);
+
+        if (dateA.isBefore(dateB)) {
             return 1;
         } else {
             return 0;
