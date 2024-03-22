c55ad8efca996c2d2ba39b1149f33c0039874161
Kim Gi Uk <2897robo@naver.com>
2024-03-22 09:29:56 +0900

    [level 0] Title: l로 만들기, Time: 20.50 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -1,14 +1,5 @@
-public class Solution {
+class Solution {
     public String solution(String myString) {
-        StringBuilder answer = new StringBuilder();
-        for(int i = 0; i < myString.length(); i++) {
-            if(myString.charAt(i) < 'l') {
-                answer.append('l');
-            }
-            else {
-                answer.append(myString.charAt(i));
-            }
-        }
-        return answer.toString();
+        return myString.replaceAll("[^l-z]", "l");
     }
 }
