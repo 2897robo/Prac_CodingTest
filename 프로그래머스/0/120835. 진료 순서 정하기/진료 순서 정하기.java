250afb32eed31e8d3011260002c6214697bbd603
Kim Gi Uk <2897robo@naver.com>
2024-04-01 10:38:32 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.02 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -1,29 +1,19 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
-public class Solution {
+class Solution {
     public int[] solution(int[] emergency) {
-        // 원래 배열의 복사본을 만들어 정렬
-        int[] sorted = emergency.clone();
-        Arrays.sort(sorted);
-        
-        // 응급도에 따른 순위를 저장할 맵 생성
-        Map<Integer, Integer> rankMap = new HashMap<>();
-        
-        // 정렬된 배열을 역순으로 순회하면서 각 응급도의 순위를 맵에 저장
-        for (int i = sorted.length - 1; i >= 0; i--) {
-            rankMap.put(sorted[i], sorted.length - i);
-        }
-        
-        // 결과를 저장할 배열 생성
         int[] answer = new int[emergency.length];
-        
-        // 원래 배열의 각 응급도에 해당하는 순위를 결과 배열에 저장
-        for (int i = 0; i < emergency.length; i++) {
-            answer[i] = rankMap.get(emergency[i]);
+
+        for(int i = 0; i < answer.length; i++){
+            if(answer[i] != 0){
+                continue;
+            }
+            int idx = 1;
+            for(int j = 0; j < answer.length; j++){
+                if(emergency[i] < emergency[j]){
+                    idx++;
+                }
+            }
+            answer[i] = idx;
         }
-        
         return answer;
     }
 }
