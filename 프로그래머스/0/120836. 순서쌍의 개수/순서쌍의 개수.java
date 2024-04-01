7d6a507b66abf06e2a3d253f1d850bc7218af300
Kim Gi Uk <2897robo@naver.com>
2024-04-01 10:42:53 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.05 ms, Memory: 66.4 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -1,21 +1,12 @@
-public class Solution {
+class Solution {
     public int solution(int n) {
-        int count = 0; // 순서쌍의 개수를 저장할 변수
-        
-        // 1부터 n까지 숫자 중에서 n을 나눌 수 있는 숫자를 찾음
-        for (int i = 1; i <= Math.sqrt(n); i++) {
-            // n을 i로 나누었을 때 나머지가 0이면 순서쌍을 찾은 것
-            if (n % i == 0) {
-                // 나누어 떨어지는 경우, 순서쌍이 하나 증가
-                count++;
-                
-                // i와 n/i가 다르면 반대 순서쌍도 존재
-                if (i != n / i) {
-                    count++;
-                }
+        if(n < 2) return n;
+       int answer = 2; // 1과 자기자신
+        for(int i=2; i<Math.sqrt(n+1); i++){
+            if(n % i == 0){
+                answer += i == n / i ? 1 : 2;
             }
         }
-        
-        return count;
+        return answer;
     }
 }
