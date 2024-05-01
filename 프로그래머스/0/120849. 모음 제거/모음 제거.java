758e9a115715c857f9c01299fa7c6bab421c2538
Kim Gi Uk <2897robo@naver.com>
2024-05-01 21:39:22 +0900

    [level 0] Title: 모음 제거, Time: 0.11 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -1,12 +1,9 @@
 class Solution {
     public String solution(String my_string) {
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < my_string.length(); i++) {
-            char c = my_string.charAt(i);
-            if (c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u') {
-                result.append(c);
-            }
-        }
-        return result.toString();
+        String answer = "";
+
+        answer = my_string.replaceAll("[aeiou]", "");
+
+        return answer;
     }
 }
