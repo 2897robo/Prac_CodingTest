ce676c664da179da6d262e3dba65ceaf3afb8d3c
Kim Gi Uk <2897robo@naver.com>
2024-05-09 19:29:44 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.02 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] solution(String[] strlist) {
+        int[] answer = new int[strlist.length]; // 결과를 저장할 배열 생성
+        for (int i = 0; i < strlist.length; i++) {
+            answer[i] = strlist[i].length(); // 각 원소의 길이를 계산하여 결과 배열에 저장
+        }
+        return answer; // 결과 배열 반환
+    }
+}
