76c739db2a92a21493223543b51da6a5302882d7
Kim Gi Uk <2897robo@naver.com>
2024-11-04 19:55:35 +0900

    [Bronze III] Title: 공 넣기, Time: 212 ms, Memory: 18584 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i=0; i<m; i++) {
+            int a = sc.nextInt() - 1;
+            int b = sc.nextInt() - 1;
+            int c = sc.nextInt();
+
+            for(int j=a; j<=b; j++) {
+                arr[j] = c;
+            }
+        }
+
+        for(int i=0; i<n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
