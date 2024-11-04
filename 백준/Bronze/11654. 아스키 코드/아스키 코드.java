e589795a5f0a5800ea98e6088d93ee3a1b71ab24
Kim Gi Uk <2897robo@naver.com>
2024-11-04 20:15:39 +0900

    [Bronze V] Title: 아스키 코드, Time: 168 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        char c = sc.nextLine().charAt(0);
+        int a = (int)c;
+        System.out.println(a);
+    }
+}
