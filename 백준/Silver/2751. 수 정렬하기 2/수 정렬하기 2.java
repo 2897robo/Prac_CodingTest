4c3d3cabd9c3023d3f2f0aa081daaede8d7dd2f4
Kim Gi Uk <2897robo@naver.com>
2024-11-20 11:48:23 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 4592 ms, Memory: 290552 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(br.readLine());
+        }
+
+        mergeSort(a);
+
+        for(int i:a) {
+            System.out.println(i);
+        }
+    }
+
+    public static void mergeSort(int[] array) {
+        if(array.length <= 1) return;
+
+        int mid = array.length/2;
+        int[] left = Arrays.copyOfRange(array, 0, mid);
+        int[] right = Arrays.copyOfRange(array, mid, array.length);
+
+        mergeSort(left);
+        mergeSort(right);
+
+        merge(array, left, right);
+    }
+
+    public static void merge(int[] array, int[] left, int[] right) {
+        int i=0, j=0, k=0;
+
+        while(i < left.length && j < right.length) {
+            if(left[i] <= right[j]) {
+                array[k++] = left[i++];
+            } else {
+                array[k++] = right[j++];
+            }
+        }
+
+        while(i < left.length) {
+            array[k++] = left[i++];
+        }
+        while(j < right.length) {
+            array[k++] = right[j++];
+        }
+    }
+}
