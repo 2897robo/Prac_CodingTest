4c7bc9f689a5927378a8ffed1434d191999cadca
Kim Gi Uk <2897robo@naver.com>
2024-11-20 16:26:41 +0900

    [Silver V] Title: 소트인사이드, Time: 104 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,33 +1,17 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String n = sc.nextLine();
-        sc.close();
-        int A[] = new int[n.length()];
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String n = br.readLine();
+        Integer[] arr = new Integer[n.length()];
         for(int i=0; i<n.length(); i++) {
-            A[i] = Integer.parseInt(n.substring(i, i+1));
+            arr[i] = n.charAt(i) - '0';
         }
 
-        // 선택정렬
-        for(int i=0; i<n.length(); i++) {
-            int max = i;
-            for(int j=i+1; j<n.length(); j++) {
-                if(A[j] > A[max]) {
-                    max = j;
-                }
-            }
+        Arrays.sort(arr, Collections.reverseOrder());
 
-            if(A[i] < A[max]) {
-                int tmp = A[i];
-                A[i] = A[max];
-                A[max] = tmp;
-            }
-        }
-
-        for(int i : A) {
-            System.out.print(i);
-        }
+        for(int i:arr) System.out.print(i);
     }
 }
