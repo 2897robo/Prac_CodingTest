8a9d0eceba4b664aef60803629f24ee9ee284fde
Kim Gi Uk <2897robo@gmail.com>
2025-02-13 18:04:59 +0900

    [Silver IV] Title: 듣보잡, Time: 268 ms, Memory: 26180 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -4,36 +4,32 @@ import java.io.*;
 public class Main {
     public static void main(String... args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        Set<String> hear = new HashSet<> ();
-        Set<String> set = new HashSet<> ();
-        int cnt = 0;
+        Set<String> hear = new HashSet<>(); // 듣도 못한 사람 저장
+        List<String> answer = new ArrayList<>(); // 듣보잡 명단 저장
 
-        for(int i=0; i<n; i++) {
+        for (int i = 0; i < n; i++) {
             hear.add(br.readLine());
         }
-        for(int j=0; j<m; j++) {
-            String tmp = br.readLine();
-            if(hear.contains(tmp)) {
-                set.add(tmp);
-                cnt++;
+
+        for (int j = 0; j < m; j++) {
+            String name = br.readLine();
+            if (hear.contains(name)) {
+                answer.add(name); // 중복이 없으므로 바로 리스트에 추가 가능
             }
         }
 
-        List<String> answer = new ArrayList<> (set);
-        Collections.sort(answer);
+        br.close();
 
-        bw.write(answer.size() + "\n");
-        for(String s : answer) {
-            bw.write(s + "\n");
+        Collections.sort(answer); // 사전순 정렬
+
+        System.out.println(answer.size());
+        for (String name : answer) {
+            System.out.println(name);
         }
-        bw.flush();
-
-        br.close(); bw.close();
     }
 }
