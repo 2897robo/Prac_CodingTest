819596e3d6fd4ac37be1fe8be45ac8a684fef1e3
Kim Gi Uk <2897robo@gmail.com>
2025-02-21 16:32:22 +0900

    [Silver II] Title: 스택 수열, Time: 296 ms, Memory: 25652 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,41 +1,47 @@
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int A[] = new int[N];
-        for(int i=0; i<N; i++) {
-            A[i] = sc.nextInt();
-        }
-        
-        sc.close();
-        Stack<Integer> stack = new Stack<>();
-        int num = 1;
-        boolean result = true;
-        StringBuffer sb = new StringBuffer();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        for(int i=0; i<N; i++) {
-            int current = A[i];
-            // 필요한 숫자까지 push
-            while(num <= current) {
-                stack.push(num++);
+        int n = Integer.parseInt(br.readLine()); // 숫자의 개수
+        int[] sequence = new int[n]; // 목표 수열
+        for (int i = 0; i < n; i++) {
+            sequence[i] = Integer.parseInt(br.readLine());
+        }
+
+        Deque<Integer> stack = new ArrayDeque<>(); // 스택 역할
+        StringBuilder sb = new StringBuilder(); // 연산 기록 (+, - 저장)
+
+        int current = 1; // 스택에 push할 숫자
+        boolean isPossible = true; // 수열을 만들 수 있는지 여부
+
+        for (int num : sequence) {
+            // 목표 숫자까지 push
+            while (current <= num) {
+                stack.push(current++);
                 sb.append("+\n");
             }
-            // 스택의 top이 현재 숫자와 다르면 수열을 만들 수 없음
-            if(stack.peek() != current) {
-                System.out.println("NO");
-                result = false;
+            // pop 해야 하는 숫자가 맞는지 확인
+            if (stack.peek() == num) {
+                stack.pop();
+                sb.append("-\n");
+            } else {
+                isPossible = false;
                 break;
             }
-            // 스택의 top이 현재 숫자와 같으면 pop
-            stack.pop();
-            sb.append("-\n");
         }
 
-        if(result) {
-            System.out.println(sb.toString());
+        if (isPossible) {
+            bw.write(sb.toString());
+        } else {
+            bw.write("NO\n");
         }
+
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
