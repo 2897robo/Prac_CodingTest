9b07da5340512f00f7c86ea3fd4030c0ecff6a6b
Kim Gi Uk <2897robo@gmail.com>
2025-03-06 16:53:20 +0900

    [Silver II] Title: 나무 자르기, Time: 1048 ms, Memory: 123816 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        long m = Long.parseLong(st.nextToken());
+        long h = 0;
+
+        st = new StringTokenizer(br.readLine(), " ");
+        long[] nArr = new long[n];
+        for(int i=0; i<n; i++) {
+            nArr[i] = Long.parseLong(st.nextToken());
+        }
+        Arrays.sort(nArr);
+
+        long left = 0;
+        long right = nArr[n-1];
+
+        while(left <= right) {
+            long mid = (left + right) / 2;
+            long sum = 0;
+
+            for(long i:nArr) {
+                if(i > mid) sum += i - mid;
+            }
+
+            if(sum >= m) {
+                h = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+
+        bw.write(h + "\n");
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
