1d42e35c190251ba28e0a27c2b163a8422a62f18
Kim Gi Uk <2897robo@gmail.com>
2025-03-23 23:07:56 +0900

    [Gold IV] Title: N-Queen, Time: 6112 ms, Memory: 15796 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,39 +1,43 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int N;
-    static int count = 0;
-    static int[] board; // board[row] = col : 각 행에 어떤 열에 퀸이 놓였는지 저장
+    static int n, answer = 0;
+    static int[] arr;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        board = new int[N];
-        
-        solve(0); // 0번째 행부터 시작
-        System.out.println(count);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+        nQueen(0);
+
+        bw.write(answer + "\n");
+        bw.flush();
+        br.close();
+        bw.close();
     }
 
-    public static void solve(int row) {
-        if (row == N) { // 퀸을 N개 모두 배치한 경우
-            count++;
+    public static void nQueen(int depth) {
+        if(depth == n) {
+            answer++;
             return;
         }
 
-        for (int col = 0; col < N; col++) {
-            board[row] = col; // 현재 행의 col열에 퀸을 놓아보기
-            if (isSafe(row)) {
-                solve(row + 1); // 다음 행으로 이동
+        for(int i=0; i<n; i++) {
+            arr[depth] = i;
+            if(isPossible(depth)) {
+                nQueen(depth+1);
             }
         }
     }
 
-    // 현재 row행에 퀸을 놓았을 때, 이전의 퀸들과 충돌이 없는지 확인
-    public static boolean isSafe(int row) {
-        for (int i = 0; i < row; i++) {
-            if (board[i] == board[row]) return false; // 같은 열
-            if (Math.abs(i - row) == Math.abs(board[i] - board[row])) return false; // 같은 대각선
+    public static boolean isPossible(int col) {
+        for(int i=0; i<col; i++) {
+            if(arr[col] == arr[i]) return false;
+            if(Math.abs(col - i) == Math.abs(arr[col] - arr[i])) return false;
         }
+
         return true;
     }
 }
