516facc0227a2894a6c27f6959c5e358b47d5221
Kim Gi Uk <2897robo@gmail.com>
2025-03-27 15:01:01 +0900

    [level 1] Title: 폰켓몬, Time: 6.93 ms, Memory: 85.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,16 +2,13 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        // 중복 제거용 Set (서로 다른 포켓몬 종류 수 체크용)
-        Set<Integer> set = new HashSet<>();
-        for (int n : nums) {
-            set.add(n);
+        Set<Integer> set = new HashSet<> ();
+        
+        for(int i:nums) {
+            set.add(i);
         }
-
-        // 최대 고를 수 있는 개수 = 전체 개수의 절반
-        int maxSelectable = nums.length / 2;
-
-        // 서로 다른 종류 수 vs 선택 가능한 수 중 더 작은 값이 정답
-        return Math.min(set.size(), maxSelectable);
+        
+        int answer = Math.min(set.size(), nums.length/2);
+        return answer;
     }
 }
