8b01694fe5a17eb46a029e3dc32146e7e2c16d2a
Kim Gi Uk <2897robo@gmail.com>
2025-04-12 23:32:11 +0900

    [Silver III] Title: N과 M (8), Time: 204 ms, Memory: 19748 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Main {
+    static int n, m;
+    static int[] input, answer;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void dfs(int depth, int start) {
+        if (depth == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(answer[i]).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = start; i < n; i++) {
+            answer[depth] = input[i];
+            dfs(depth + 1, i);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        input = new int[n];
+        answer = new int[m];
+
+        for (int i = 0; i < n; i++) {
+            input[i] = sc.nextInt();
+        }
+
+        Arrays.sort(input);
+        dfs(0, 0);
+        System.out.print(sb);
+    }
+}
