2f85c262a516eb563b538dd531519fece49aa880
Kim Gi Uk <2897robo@gmail.com>
2025-06-10 01:19:53 +0900

    [level 1] Title: 신규 아이디 추천, Time: 13.11 ms, Memory: 86.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String solution(String new_id) {
+        String answer = new_id;
+        
+        answer = new_id.toLowerCase();
+        answer = answer.replaceAll("[^a-z0-9\\-_.]", "");
+        answer = answer.replaceAll("\\.{2,}", ".");
+        if(answer.startsWith(".")) {
+            answer = answer.substring(1);
+        }
+        if(answer.endsWith(".")) {
+            answer = answer.substring(0, answer.length() - 1);
+        }
+        
+        if(answer.isEmpty()) {
+            answer = "a";
+        }
+        
+        if(answer.length() >= 16) {
+            answer = answer.substring(0, 15);
+            if(answer.endsWith(".")) {
+                answer = answer.substring(0, answer.length()-1);
+            }
+        }
+        
+        while(answer.length() <= 2) {
+            answer += answer.charAt(answer.length() - 1);
+        }
+        
+        return answer;
+    }
+}
