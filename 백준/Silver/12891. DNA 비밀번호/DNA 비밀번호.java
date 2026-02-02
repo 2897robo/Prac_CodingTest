2a1540989b83b5dbcf5211509ca6901c83d2309f
Kim Gi Uk <2897robo@gmail.com>
2026-02-02 11:32:27 +0900

    [Silver II] Title: DNA 비밀번호, Time: 212 ms, Memory: 23368 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -1,68 +1,81 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+// BOJ 12891 - DNA 비밀번호
+
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int S;
-    static int P;
-    static int[] CheckArr = new int[4];
-    static int[] myArr = new int[4];
-    static int checkCount = 0;
-    static int answer = 0;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        S = Integer.parseInt(st.nextToken());
-        P = Integer.parseInt(st.nextToken());
 
-        char[] DnaArr = br.readLine().toCharArray();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int s = Integer.parseInt(st.nextToken());
+        int p = Integer.parseInt(st.nextToken());
+        char[] dna = br.readLine().toCharArray();
 
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < 4; i++) {
-            CheckArr[i] = Integer.parseInt(st.nextToken());
-        }
+        int requiredA = Integer.parseInt(st.nextToken());
+        int requiredC = Integer.parseInt(st.nextToken());
+        int requiredG = Integer.parseInt(st.nextToken());
+        int requiredT = Integer.parseInt(st.nextToken());
+        int countA = 0, countC = 0, countG = 0, countT = 0;
 
-        for(int i=0; i<P; i++) {
-            if(DnaArr[i] == 'A') myArr[0]++;
-            if(DnaArr[i] == 'C') myArr[1]++;
-            if(DnaArr[i] == 'G') myArr[2]++;
-            if(DnaArr[i] == 'T') myArr[3]++;
-        }
-
-        if(checkCounting()) {
-            answer++;
-        }
-
-        int i = -1;
-        for(int j=P; j<S; j++) {
-            i = j-P;
-
-            if(DnaArr[i] == 'A') myArr[0]--;
-            if(DnaArr[i] == 'C') myArr[1]--;
-            if(DnaArr[i] == 'G') myArr[2]--;
-            if(DnaArr[i] == 'T') myArr[3]--;
-
-            if(DnaArr[j] == 'A') myArr[0]++;
-            if(DnaArr[j] == 'C') myArr[1]++;
-            if(DnaArr[j] == 'G') myArr[2]++;
-            if(DnaArr[j] == 'T') myArr[3]++;
-
-            if(checkCounting()) {
-                answer++;
+        for (int i = 0; i < p; i++) {
+            switch (dna[i]) {
+                case 'A':
+                    countA++;
+                    break;
+                case 'C':
+                    countC++;
+                    break;
+                case 'G':
+                    countG++;
+                    break;
+                case 'T':
+                    countT++;
+                    break;
             }
         }
 
-        System.out.println(answer);
-    }
+        int validPasswords = 0;
+        if (countA >= requiredA && countC >= requiredC && countG >= requiredG && countT >= requiredT) {
+            validPasswords++;
+        }
 
-    public static boolean checkCounting() {
-        for(int i=0; i<4; i++) {
-            if(myArr[i] < CheckArr[i]) {
-                return false;
+        for (int i = p; i < s; i++) {
+            switch (dna[i - p]) {
+                case 'A':
+                    countA--;
+                    break;
+                case 'C':
+                    countC--;
+                    break;
+                case 'G':
+                    countG--;
+                    break;
+                case 'T':
+                    countT--;
+                    break;
+            }
+
+            switch (dna[i]) {
+                case 'A':
+                    countA++;
+                    break;
+                case 'C':
+                    countC++;
+                    break;
+                case 'G':
+                    countG++;
+                    break;
+                case 'T':
+                    countT++;
+                    break;
+            }
+
+            if (countA >= requiredA && countC >= requiredC && countG >= requiredG && countT >= requiredT) {
+                validPasswords++;
             }
         }
-        return true;
+        System.out.println(validPasswords);
     }
 }
