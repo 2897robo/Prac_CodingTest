9cb35b98018a686ced1b0aa81a957cf31af2e2a5
Kim Gi Uk <2897robo@gmail.com>
2026-02-03 22:29:00 +0900

    [Silver IV] Title: 수들의 합 2, Time: 120 ms, Memory: 14908 KB -BaekjoonHub

---
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -0,0 +1,39 @@
+// BOJ 2003 - 수들의 합 2
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int start = 0, end = 0, sum = 0, count = 0;
+
+        while (true) {
+            if (sum >= m) {
+                sum -= arr[start++];
+            } else if (end == n) {
+                break;
+            } else {
+                sum += arr[end++];
+            }
+
+            if (sum == m) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+        br.close();
+    }
+}
