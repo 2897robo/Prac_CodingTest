aaea572c962a255e6dc6dcc1e6dcbfee935dbae1
Kim Gi Uk <2897robo@gmail.com>
2026-02-04 15:02:33 +0900

    [Silver V] Title: 색종이, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,32 +1,31 @@
-import java.util.Scanner;
+// BOJ 2563 - 색종이
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        // 입력을 받기 위한 Scanner 객체 생성
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        // 도화지의 크기는 100x100
         int canvasSize = 100;
         boolean[][] canvas = new boolean[canvasSize][canvasSize];
 
-        // 색종이의 수를 입력받음
-        int numPapers = scanner.nextInt();
+        int numPapers = Integer.parseInt(br.readLine());
 
-        // 각 색종이의 위치를 입력받아 도화지에 표시
         for (int i = 0; i < numPapers; i++) {
-            int x = scanner.nextInt();
-            int y = scanner.nextInt();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
 
-            // 색종이는 10x10 크기
             for (int j = x; j < x + 10; j++) {
                 for (int k = y; k < y + 10; k++) {
-                    // 해당 위치를 true로 표시 (색종이가 붙어 있음을 표시)
                     canvas[j][k] = true;
                 }
             }
         }
 
-        // 검은 영역의 넓이를 계산
         int blackArea = 0;
         for (int i = 0; i < canvasSize; i++) {
             for (int j = 0; j < canvasSize; j++) {
@@ -36,10 +35,6 @@ public class Main {
             }
         }
 
-        // 결과 출력
         System.out.println(blackArea);
-
-        // Scanner 객체 닫기
-        scanner.close();
     }
 }
