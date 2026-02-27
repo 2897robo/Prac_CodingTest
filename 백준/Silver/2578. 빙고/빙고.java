822f16de612be85633ab59b16e7531c60abc36d7
Kim Gi Uk <2897robo@gmail.com>
2026-02-27 14:56:31 +0900

    [Silver IV] Title: 빙고, Time: 100 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/2578. 빙고/빙고.java
+++ 백준/Silver/2578. 빙고/빙고.java
@@ -3,80 +3,77 @@
 import java.util.*;
 import java.io.*;
 public class Main {
-    static int[][] map = new int[5][5];
     static int[][] visited = new int[5][5];
+    static Map<Integer, int[]> position = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 5; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 5; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
+        
+        // 빙고판 읽기 및 위치 저장
         for (int i = 0; i < 5; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < 5; j++) {
                 int num = Integer.parseInt(st.nextToken());
-                for (int k = 0; k < 5; k++) {
-                    for (int l = 0; l < 5; l++) {
-                        if (map[k][l] == num) {
-                            visited[k][l] = 1;
-                        }
-                    }
-                }
-                if (check()) {
-                    sb.append(i * 5 + j + 1);
-                    break;
-                }
-            }
-            if (sb.length() > 0) {
-                break;
+                position.put(num, new int[]{i, j});
             }
         }
 
-        System.out.println(sb);
-
+        // 불리는 숫자 처리
+        int count = 0;
+        for (int i = 0; i < 5; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 5; j++) {
+                int num = Integer.parseInt(st.nextToken());
+                count++;
+                
+                // 이미 맵에 있는 숫자면 체크
+                if (position.containsKey(num)) {
+                    int[] pos = position.get(num);
+                    visited[pos[0]][pos[1]] = 1;
+                }
+                
+                // 빙고 완성되면 출력
+                if (count > 3 && check()) {
+                    System.out.println(count);
+                    return;
+                }
+            }
+        }
     }
 
     static boolean check() {
         int count = 0;
+        
+        // 행 체크
         for (int i = 0; i < 5; i++) {
+            if (countSum(visited[i]) == 5) count++;
+        }
+        
+        // 열 체크
+        for (int j = 0; j < 5; j++) {
             int sum = 0;
-            for (int j = 0; j < 5; j++) {
+            for (int i = 0; i < 5; i++) {
                 sum += visited[i][j];
             }
-            if (sum == 5) {
-                count++;
-            }
+            if (sum == 5) count++;
         }
-
+        
+        // 대각선 체크
+        int diag1 = 0, diag2 = 0;
         for (int i = 0; i < 5; i++) {
-            int sum = 0;
-            for (int j = 0; j < 5; j++) {
-                sum += visited[j][i];
-            }
-            if (sum == 5) {
-                count++;
-            }
+            diag1 += visited[i][i];
+            diag2 += visited[i][4 - i];
         }
-
-        int sum1 = 0;
-        int sum2 = 0;
-        for (int i = 0; i < 5; i++) {
-            sum1 += visited[i][i];
-            sum2 += visited[i][4 - i];
-        }
-        if (sum1 == 5) {
-            count++;
-        }
-        if (sum2 == 5) {
-            count++;
-        }
-
+        if (diag1 == 5) count++;
+        if (diag2 == 5) count++;
+        
         return count >= 3;
+    }
+    
+    static int countSum(int[] arr) {
+        int sum = 0;
+        for (int x : arr) sum += x;
+        return sum;
     }
 
 }
