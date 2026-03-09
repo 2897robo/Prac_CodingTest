9b7ec0e7d9d0f0f6bbd4825f2afd3baaffe3f9ec
Kim Gi Uk <2897robo@gmail.com>
2026-03-09 14:43:29 +0900

    [Gold IV] Title: 사이클 게임, Time: 620 ms, Memory: 125292 KB -BaekjoonHub

---
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.java
@@ -0,0 +1,49 @@
+// BOJ 20040 사이클 게임
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[] parent;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        
+        parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+        
+        for (int i = 1; i <= m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (find(a) == find(b)) {
+                System.out.println(i);
+                return;
+            }
+            
+            union(a, b);
+        }
+        
+        System.out.println(0);
+    }
+
+    static int find(int x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    static void union(int a, int b) {
+        int rootA = find(a);
+        int rootB = find(b);
+        if (rootA != rootB) {
+            parent[rootB] = rootA;
+        }
+    }
+}
